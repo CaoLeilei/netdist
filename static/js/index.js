a9a48cdfd5b5b4568b8b80947fac9110d2b9209a
siyuan <caosiyuan@huobi.com>
2022-12-27 19:14:33 +0800

    部分内容的修改与开发

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+(function () {
+    const App = {
+        data() {
+            return {
+                message: "Hello Element Plus",
+            };
+        },
+    };
+    const app = Vue.createApp(App);
+    app.use(ElementPlus);
+    app.mount("#app");
+    // ElementPlus
+})()
